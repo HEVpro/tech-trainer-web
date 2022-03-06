18a8a90d4b7d1cd36d4b4a6bcbacdbc58d976f34
mouad-boubkari <mouadboubkari365@gmail.com>
2022-03-06 14:36:03 +0100

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
 export const Navbar = () => {
   return (
     <nav className="bg-[#C4C4C4] flex items-center justify-between h-24 ">
-      <div className="flex items-center ml-14 border-2 border-black">
-        <a href="#" className="content-center text-center w-20 h-12">
-          MK
+      <div className="ml-14">
+        <a href="#">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C560BAQG5KBpL2YeNwA/company-logo_200_200/0/1646384462808?e=2147483647&v=beta&t=sPtKg5U3IOzKkyfkg0kMyShJNsKh1DoACq1LGanD_34"
+            alt="MK"
+            className="w-20 h-14"
+          />
         </a>
       </div>
 
-      <div className="flex justify-center space-x-20 content-center">
+      <div className="flex justify-center space-x-5 content-center">
         <a
           href="#"
           className="bg-[#1F1C1D] text-[#F4F1ED] text-center w-48 h-7 rounded"
         >
           Project
         </a>
-
         <a
           href="#"
           className="bg-[#1F1C1D] text-[#F4F1ED] text-center w-48 h-7 rounded"
         >
           Technologies
         </a>
-
         <a
           href="#"
           className="bg-[#1F1C1D] text-[#F4F1ED] text-center w-48 h-7 rounded"
@@ -30,12 +33,10 @@ export const Navbar = () => {
         </a>
       </div>
 
-      <div className="flex justify-end items-center mr-14">
-        <a href="#">
-          <button className="bg-[#FF9443] text-[#1F1C1D] w-48 h-12  rounded-2xl">
-            Contact
-          </button>
-        </a>
+      <div className="mr-14">
+        <button className="bg-[#FF9443] text-[#1F1C1D] w-48 h-12  rounded-2xl">
+          <a href="#">Contact</a>
+        </button>
       </div>
     </nav>
   );
