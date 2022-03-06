429aa5ab97e694df76690eb621d1f9e181cb3b96
mouad-boubkari <mouadboubkari365@gmail.com>
2022-03-06 15:51:22 +0100

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 export const Navbar = () => {
   return (
-    <nav className="bg-[#C4C4C4] flex items-center justify-between h-24 ">
+    <nav className="flex items-center justify-between bg-[#C4C4C4] h-24">
       <div className="ml-14">
         <a href="#">
           <img
@@ -12,7 +12,7 @@ export const Navbar = () => {
         </a>
       </div>
 
-      <div className="flex justify-center space-x-5 content-center">
+      <div className="md:flex justify-center space-x-5 content-center">
         <a
           href="#"
           className="bg-[#1F1C1D] text-[#F4F1ED] text-center w-48 h-7"
@@ -34,7 +34,7 @@ export const Navbar = () => {
       </div>
 
       <div className="mr-14">
-        <button className="bg-[#FF9443] text-[#1F1C1D] w-48 h-12  rounded-2xl">
+        <button className="bg-[#FF9443] text-[#1F1C1D] w-48 h-12 rounded-2xl">
           <a href="#">Contact</a>
         </button>
       </div>
