4a5dd23a834c756bcf3c688b63d61ee0bcf436c1
mouad-boubkari <mouadboubkari365@gmail.com>
2022-03-08 20:22:53 +0100

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 export const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between  bg-[#C4C4C4] sm:h-16 lg:h-24">
+    <nav className="flex items-center justify-between bg-[#C4C4C4] sm:h-16 lg:h-24">
       <div className="sm:ml-5 lg:ml-14">
-        <a href="#"><img src="https://assets.stickpng.com/images/584a9b3bb080d7616d298777.png" alt="MK" className="sm:w-10 sm:h10 lg:w-16 lg:h-16"/></a>
+          <a href="#"><img src="https://assets.stickpng.com/images/584a9b3bb080d7616d298777.png" alt="MK" className="sm:w-10 sm:h10 lg:w-16 lg:h-16"/></a>
       </div>
       <div className="flex justify-center space-x-5 content-center">
         <a href="#" className="bg-[#1F1C1D] text-[#F4F1ED] text-center w-48 h-7">Project</a>
