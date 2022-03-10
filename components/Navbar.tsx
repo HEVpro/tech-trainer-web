b31d4999e14662c1e0c33959eaa678a490b8f310
hector <hector.evalverde@gmail.com>
2022-03-10 13:26:47 +0100

    feat: useState handle menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,34 @@
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {useState} from "react";
+
 
 export const Navbar = () => {
+    //TExt cases
+    // dani_valer snake case
+    // daniValero camel case
+    // dani-valero kebab case
 
-  const letras = ["Project", "Methodology", "Technologies", "Communication", "Pricing"];
+    const [menu, setMenu] = useState<boolean>(false)
+
+    const letras = ["Project", "Methodology", "Technologies", "Communication", "Pricing"];
+
+    const handleMenu = () => {
+        if(!menu){
+            setMenu(true)
+        }else{
+            setMenu(false)
+        }
+    }
+    console.log(menu)
 
   return (
-    <nav className="flex items-center justify-evenly bg-[#C4C4C4] w-full p-2 h-24">
-          <div className="flex items-center justify-center w-16 h-12">
-            <FontAwesomeIcon icon={faCoffee} />
-          </div>
+    <nav className="flex items-center justify-evenly bg-secondary-grey w-full p-2 h-24">
+          <button className="flex items-center justify-center w-16 h-12" onClick={() => handleMenu()}>
+              {menu ?
+                  <FontAwesomeIcon className="" icon={faCircleXmark} /> :
+                  <FontAwesomeIcon className="" icon={faBars} />}
+          </button>
         <div className="flex justify-center space-x-5 content-center">
           {letras.map(letra => (
               <div key={letra} className="flex items-center justify-center rounded-lg w-32 h-10 bg-secondary-black">
