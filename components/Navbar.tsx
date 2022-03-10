c2a2114754bf2dde2c4229d83f1375d8b947dbd2
hector <hector.evalverde@gmail.com>
2022-03-10 10:07:53 +0100

    feat: fontawesome

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,15 @@
-/* eslint-disable @next/next/no-img-element */
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export const Navbar = () => {
   return (
     <nav className="flex items-center justify-between bg-[#C4C4C4] sm:h-16 lg:h-24">
       <div className="hidden sm:block">
         <div className="sm:ml-5 lg:ml-14">
-          <a href="#">
-            <img
-              src="https://assets.stickpng.com/images/584a9b3bb080d7616d298777.png"
-              alt="MK"
-              className="w-8 h-8 sm:w-10 sm:h10 lg:w-16 lg:h-16"
-            />
-          </a>
+          <div className="w-12 mt-10">
+            <FontAwesomeIcon icon={faCoffee} />
+          </div>
+
         </div>
         <div className="flex justify-center space-x-5 content-center">
           <a
